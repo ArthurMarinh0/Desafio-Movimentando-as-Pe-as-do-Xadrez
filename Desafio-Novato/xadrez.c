d971a029c6531b37a51fd9a28fc16097c4944f73
Arthur Marinho <arthurmarinho.201556@gmail.com>
2025-09-24 13:42:44 -0300

    Reelendo e comentando o codigo

--- Desafio-Novato/xadrez.c
+++ Desafio-Novato/xadrez.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 int main(){
-
+    //Definindo o número de casas que cada peça irá se mover.
     const int casasTorre = 5;
     const int casasBispo = 5;
-    const int casasRainha = 5;
+    const int casasRainha = 8;
 
 
 //Movimentação da Torre
-
+    // A Torre se move em linha reta. Vamos simular o movimento de 5 casas para a direita.
     printf("***Movimento da Torrez***\n");
     for(int i = 0; i < casasTorre; i++){
 
@@ -16,7 +16,7 @@ int main(){
     }
 
 //Movimentação do Bispo
-    
+    // O Bispo se move na diagonal. Vamos simular o movimento de 5 casas na diagonal para cima e para a direita.
     printf("\n***Movimento do Bispo***\n");
 
     int contadorBispo = 0;
@@ -27,7 +27,7 @@ int main(){
     }
 
 //Movimentação da Rainha
-
+    //A Rainha pode se mover em todas as direções. Vamos simular o movimento de 8 casas para a esquerda.
     printf("\n***Movimento da Rainha***\n");
 
     int contadorRainha = 0;
